38f06ac3981a6bce626bf22ad1ac6ccd8a4106ee
Gayan Pathirana <gayanpathira@gmail.com>
2019-10-17 21:04:25 +0530

    adding base files to repo

---
+++ resources/js/components/Statistics.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-body chart-container">
+            <charts :options="chartOptions" :updateArgs="updateArgs"></charts>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Highcharts from "highcharts";
+export default {
+  data() {
+    return {
+      updateArgs: [true, true, true],
+      chartOptions: {
+        chart: {
+          type: "line",
+          zoomType: "x"
+        },
+        plotOptions: {
+          line: {
+            marker: {
+              enabled: false
+            }
+          },
+          series: {
+            pointPlacement: "on"
+          }
+        },
+        title: {
+          text: "Weekly Onbording Retention Curve"
+        },
+        subtitle: {
+          text: "By Gayan Pathirana"
+        },
+        yAxis: {
+          min: 0,
+          max: 100,
+          title: {
+            text: "%"
+          }
+        },
+        legend: {
+          layout: "vertical",
+          align: "right",
+          verticalAlign: "middle"
+        },
+        xAxis: {
+          tickmarkPlacement: "on",
+          startOnTick: true,
+          categories: [
+            "Step 1 (0%)",
+            "Step 2 (20%)",
+            "Step 3 (40%)",
+            "Step 4 (50%)",
+            "Step 5 (70%)",
+            "Step 6 (90%)",
+            "Step 7 (99%)",
+            "Step 8 (100%)"
+          ]
+        },
+
+        series: [{}]
+      },
+      title: ""
+    };
+  },
+  beforeMount() {
+    let self = this;
+    axios.get("/api/user-chart-data").then(response => {
+      self.chartOptions.series = response.data;
+    });
+  },
+  mounted() {}
+};
+</script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Temper.works - Test</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
+        <!--Basic Styles -->
         <style>
             html, body {
                 background-color: #fff;
@@ -22,12 +19,13 @@
 
             .full-height {
                 height: 100vh;
+                width:80vw;
             }
 
             .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
+                display:block;
+                margin:0 auto;
+                padding:10% 0px;
             }
 
             .position-ref {
@@ -44,57 +42,16 @@
                 text-align: center;
             }
 
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
         </style>
     </head>
     <body>
+    <div id='app'>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <statistics-component></statistics-component>
             </div>
         </div>
+    </div>
+    <script src="{{ mix('js/app.js') }}"></script>
     </body>
 </html>
